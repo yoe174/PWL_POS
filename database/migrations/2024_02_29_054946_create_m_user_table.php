78605849fef17b91786a7a214740cb7854d2e653
yoe174 <yoechannelpjr@gmail.com>
2024-02-29 14:11:42 +0700

    praktikum 2.2

---
+++ database/migrations/2024_02_29_054946_create_m_user_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('m_user', function (Blueprint $table) {
+            $table->id('user_id');
+            $table->unsignedBigInteger('level_id')->index(); // index untuk foreignkey
+            $table->string('username', 20)->unique(); // unique untuk memastikan tidak ada username yang sama
+            $table->string('nama', 100);
+            $table->string('password', 255);
+
+            // mendefenisikan foreign key pada kolom_id mengacu pada kolom level_ide di tabel m_level
+            $table->foreign('level_id')->references('level_id')->on('m_level');
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('m_user');
+    }
+};
