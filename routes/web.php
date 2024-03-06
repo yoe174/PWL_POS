ae368abf911ce1a17de104e44ad8aa6e9ae139d2
yoe174 <yoechannelpjr@gmail.com>
2024-03-06 23:09:41 +0700

    praktikum 5 Query Buildder

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\LevelController;
 use Illuminate\Support\Facades\Route;
 
@@ -18,4 +19,5 @@ Route::get('/', function () {
 });
 
 Route::get('/level', [LevelController::class, 'index']);
+Route::get('/kategori', [KategoriController::class, 'index']);
 
