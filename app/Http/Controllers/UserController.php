66cc011cda7497b10ac712110fc67f72f92b1d3e
yoe174 <yoechannelpjr@gmail.com>
2024-03-07 12:56:40 +0700

    JS4 Praktikum 1

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -21,10 +21,24 @@ class UserController extends Controller
         // ];
         // UserModel::insert($data);
 
+        // $data = [
+        //     'nama' => 'Pelanggan Pertama',
+        // ];
+        // UserModel::where('username','customer-1')->update($data);
+
+        // $data = [
+        //     'level_id' => 2,
+        //     'username' => 'manager-2',
+        //     'nama' => 'Manager',
+        //     'password' => Hash::make('12345'),
+        // ];
         $data = [
-            'nama' => 'Pelanggan Pertama',
+            'level_id' => 2,
+            'username' => 'manager_tiga2',
+            'nama' => 'Manager 3',
+            'password' => Hash::make('12345'),
         ];
-        UserModel::where('username','customer-1')->update($data);
+        UserModel::create($data);
 
         $user = UserModel::all();
         return view('user',['data' => $user]);
