6c3ec2eb953ae50ceab619d5738e5d0240df3cbc
yoe174 <yoechannelpjr@gmail.com>
2024-03-13 19:09:29 +0700

    JS4 Praktikum 2.3

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -63,7 +63,12 @@ class UserController extends Controller
         // praktikum 2.2
         // $user = UserModel::findOrFail(1);
         // return view('user',['data' => $user]);
-        $user = UserModel::where('username','manager9')->firstOrFail();
+        // $user = UserModel::where('username','manager9')->firstOrFail();
+        // return view('user',['data' => $user]);
+
+        // Praktikum 2.3
+        $user = UserModel::where('level_id',2)->count();
+        // dd($user);
         return view('user',['data' => $user]);
     }
 }
