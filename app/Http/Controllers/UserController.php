3dcfe366da953609cb8d610f7fff2cbbe165d44a
yoe174 <yoechannelpjr@gmail.com>
2024-03-13 18:47:39 +0700

    JS4 Praktikum 2.2

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -55,10 +55,15 @@ class UserController extends Controller
         //     abort(404);
         // });
         // return view('user', ['data' => $user]);
-        $user = UserModel::findOr(20,['username','nama'], function (){
-            abort(404);
-        });
-        return view('user', ['data' => $user]);
+        // $user = UserModel::findOr(20,['username','nama'], function (){
+        //     abort(404);
+        // });
+        // return view('user', ['data' => $user]);
 
+        // praktikum 2.2
+        // $user = UserModel::findOrFail(1);
+        // return view('user',['data' => $user]);
+        $user = UserModel::where('username','manager9')->firstOrFail();
+        return view('user',['data' => $user]);
     }
 }
