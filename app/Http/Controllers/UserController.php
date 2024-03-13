b6543b02f3656b88e4ea75f4c7ac2fddc6aa4e50
yoe174 <yoechannelpjr@gmail.com>
2024-03-13 20:17:49 +0700

    JS4 Praktikum 2.4

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -67,8 +67,45 @@ class UserController extends Controller
         // return view('user',['data' => $user]);
 
         // Praktikum 2.3
-        $user = UserModel::where('level_id',2)->count();
+        // $user = UserModel::where('level_id',2)->count();
         // dd($user);
-        return view('user',['data' => $user]);
+        // return view('user',['data' => $user]);
+
+        // Praktikum 2.4
+        // $user = UserModel::firstOrCreate(
+        //     [
+        //         'username' => 'manager',
+        //         'nama' => 'Manager',
+        //     ]
+        //     );
+        //     return view('user', ['data' => $user]);
+        // $user = UserModel::firstOrCreate(
+        //     [
+        //         'username' => 'manager22',
+        //         'nama' => 'Manager Dua Dua',
+        //         'password' => Hash::make('12345'),
+        //         'level_id' => 2,
+        //     ]
+        //     );
+        // $user = UserModel::firstOrNew(
+        //     [
+        //         'username' => 'manager',
+        //         'nama' => 'Manager',
+        //     ],
+        // );
+        // return view('user', ['data' => $user]);
+
+        $user = UserModel::firstOrNew(
+            [
+                'username' => 'manager33',
+                'nama' => 'Manager Tiga Tiga',
+                'password' => Hash::make('12345'),
+                'level_id' => 2
+            ],
+        );
+        $user->save();
+
+        return view('user', ['data' => $user]);
     }
+
 }
--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -6,11 +6,11 @@
         <h1>Data User</h1>
         <table border="1" cellpadding="2" cellspacing="0">
             <tr>
-                {{-- <th>ID</th>
+                <th>ID</th>
                 <th>Username</th>
                 <th>Nama</th>
-                <th>ID Level Pengguna</th> --}}
-                <th>Jumlah Pengguna</th>
+                <th>ID Level Pengguna</th>
+                
             </tr>
             {{-- @foreach ($data as $d)
             <tr>
@@ -21,11 +21,11 @@
             </tr>    
             @endforeach --}}
             <tr>
-                {{-- <td>{{ $data->user_id}}</td>
+                <td>{{ $data->user_id}}</td>
                 <td>{{ $data->username}}</td>
                 <td>{{ $data->nama}}</td>
-                <td>{{ $data->level_id}}</td> --}}
-                <td>{{$data}}</td>
+                <td>{{ $data->level_id}}</td>
+                {{-- <td>{{$data}}</td> --}}
             </tr>
         </table>
     </body>
