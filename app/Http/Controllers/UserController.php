8c9bb69ce765ea4a03202942d7b0382d23203eb5
yoe174 <yoechannelpjr@gmail.com>
2024-03-13 18:35:01 +0700

    JS4 Praktikum 2.1

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -32,15 +32,33 @@ class UserController extends Controller
         //     'nama' => 'Manager',
         //     'password' => Hash::make('12345'),
         // ];
-        $data = [
-            'level_id' => 2,
-            'username' => 'manager_tiga2',
-            'nama' => 'Manager 3',
-            'password' => Hash::make('12345'),
-        ];
-        UserModel::create($data);
+        // $data = [
+        //     'level_id' => 2,
+        //     'username' => 'manager_tiga2',
+        //     'nama' => 'Manager 3',
+        //     'password' => Hash::make('12345'),
+        // ];
+        // UserModel::create($data);
 
-        $user = UserModel::all();
-        return view('user',['data' => $user]);
+        // $user = UserModel::all();
+        // return view('user',['data' => $user]);
+
+        // PRAKTIKUM 2
+        // $user = UserModel::find(1);
+        // $user = UserModel::where('level_id',3)->first();
+        // return view('user',['data' => $user]);
+
+        // $user = UserModel::firstWhere('level_id',1);
+        // return view('user',['data' => $user]);
+
+        // $user = UserModel::findOr(1,['username','nama'], function (){
+        //     abort(404);
+        // });
+        // return view('user', ['data' => $user]);
+        $user = UserModel::findOr(20,['username','nama'], function (){
+            abort(404);
+        });
+        return view('user', ['data' => $user]);
+
     }
 }
