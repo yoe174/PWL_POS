e46da296ca75a656daf93564f013b25389ef5d51
yoe174 <yoechannelpjr@gmail.com>
2024-04-29 20:27:43 +0700

    JS10 prak.3

---
+++ app/Http/Controllers/Api/LogoutController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Tymon\JWTAuth\Facades\JWTAuth;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Tymon\JWTAuth\ExceptionsTokenExpiredException;
+use Tymon\JWTAuth\ExceptionsTokenInvalidException;
+
+class LogoutController extends Controller
+{
+    public function __invoke(Request $request)
+    {
+        //remove token
+        $removeToken = JWTAuth::invalidate(JWTAuth::getToken());
+
+        if ($removeToken) {
+            //return response JSON
+            return response()->json([
+                'success' => true,
+                'message' => 'Logout Berhasil!',
+            ]);
+        }
+    }
+}
