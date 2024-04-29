d46dc616a5b9053fb2eba71436707a7de64230ec
yoe174 <yoechannelpjr@gmail.com>
2024-04-29 19:23:24 +0700

    JS10 prak.1

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -6,11 +6,22 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
 // class UserModel extends Model
-class UserModel extends Authenticatable
+class UserModel extends Authenticatable implements JWTSubject
 {
     use HasFactory;
+
+    public function getJWTIdentifier()
+    {
+        return $this->getkey();
+    }
+
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
 
     protected $table = 'm_user';
     protected $primaryKey = 'user_id';
